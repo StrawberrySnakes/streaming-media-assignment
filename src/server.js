d6d84108ef06051cfad56b52ac792dac720293f8
DJ <djs9826@rit.edu>
2026-01-21 17:34:59 -0500

    commenting

--- src/server.js
+++ src/server.js
@@ -5,25 +5,26 @@ const mediaHandler = require('./mediaResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-  console.log(request.url);
-
-  switch (request.url) {
-    case '/':
-      htmlHandler.getIndex(request, response);
-      break;
-    case '/party.mp4':
-      mediaHandler.getParty(request, response);
-      break;
-    case '/bling.mp3':
-      mediaHandler.getBling(request, response);
-      break;
-    case '/bird.mp4':
-      mediaHandler.getBird(request, response);
-      break;
-    default:
-      htmlHandler.getIndex(request, response);
-      break;
-  }
+  // Not needed
+    console.log(request.url);
+    //different pages
+    switch (request.url) {
+        case '/':
+            htmlHandler.getIndex(request, response);
+            break;
+        case '/party.mp4':
+            mediaHandler.getParty(request, response);
+            break;
+        case '/bling.mp3':
+            mediaHandler.getBling(request, response);
+            break;
+        case '/bird.mp4':
+            mediaHandler.getBird(request, response);
+            break;
+        default:
+            htmlHandler.getIndex(request, response);
+            break;
+    }
 };
 
 http.createServer(onRequest).listen(port, () => {
