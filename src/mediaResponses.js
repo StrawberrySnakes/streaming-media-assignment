1383b816cb974847f53788015d021ddd19be7637
DJ <djs9826@rit.edu>
2026-01-21 17:31:50 -0500

    adding bling and bird files

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
-const { request } = require('http');
 const path = require('path');
 
-const getParty = (request, response) => {
-  const file = path.resolve(__dirname, '../client/party.mp4');
+const loadFile = (request, response, filePath, fileType) => {
+  const file = path.resolve(__dirname, filePath);
   fs.stat(file, (err, stats) => {
     if (err) {
       if (err.code === 'ENOENT') {
@@ -11,6 +10,7 @@ const getParty = (request, response) => {
       }
       return response.end(err);
     }
+
     let { range } = request.headers;
 
     if (!range) {
@@ -32,7 +32,7 @@ const getParty = (request, response) => {
       'Content-Range': `bytes ${start}-${end}/${total}`,
       'Accent-Ranges': 'bytes',
       'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
+      'Content-Type': fileType,
     });
 
     const stream = fs.createReadStream(file, { start, end });
@@ -49,4 +49,20 @@ const getParty = (request, response) => {
   });
 };
 
-module.exports.getParty = getParty;
+const getParty = (request, response) => {
+  loadFile(request, response, '../client/party.mp4', 'video/mp4');
+};
+
+const getBling = (request, response) => {
+  loadFile(request, response, '../client/bling.mp3', 'audio/mpeg');
+};
+
+const getBird = (request, response) => {
+  loadFile(request, response, '../client/bird.mp4', 'video/mp4');
+};
+
+module.exports = {
+  getParty,
+  getBling,
+  getBird,
+};
